1aaa486899921b89960983b0b025eb4731e5d0b2
hojinlee <infohojin@gmail.com>
2020-03-26 19:01:42 +0900

    오토로드3
    추출한 클래스 파일을 require 합니다.

--- loading.php
+++ loading.php
@@ -3,11 +3,16 @@
 // 지니모듈 오토로드
 spl_autoload_register(function($className) {
     echo $className."을 로드 합니다.\n";
-    
+
     // 클래스 파일명 변환
     $path = str_replace("\\", DIRECTORY_SEPARATOR, $className).".php";
     $path = __DIR__.DIRECTORY_SEPARATOR.$path;
     echo $path;
 
-    exit;
+    if(file_exists($path)) {
+        require_once($path);
+    } else {
+        echo "Module loading: ".$path." file not found\n";
+        exit;
+    }
 });
