b9977d970f158d37b3703d28e4240451a8313a7f
hojinlee <infohojin@gmail.com>
2020-03-26 18:53:25 +0900

    데이터베이스 모듈2
    PDO 접속처리 => 모듈로 이동함.

--- Modules/Databases/Database.php
+++ Modules/Databases/Database.php
@@ -9,6 +9,28 @@ class Database
 {
     public function __construct()
     {
-        echo __CLASS__;
+        $host = "mysql:";
+        $host .= "host=localhost";
+        $host .= ";dbname=shop";
+        $host .= ";charser=utf8";
+
+        $dbuser = "db2020";
+        $dbpassword = "123456";
+
+        // echo __CLASS__;
+        if (extension_loaded("PDO") && extension_loaded("pdo_mysql")) {
+
+            try {
+                $conn = new \PDO($host, $dbuser, $dbpassword);
+                echo "데이터 베이스 접속 성공!\n";
+            } catch (PDOException $e) {
+                echo "접속 실패\n";
+                echo $e->getMessage();
+            }
+        
+        } else {
+            echo "PDO 드라이버가 활성화 되어 있지 않습니다.\n";
+            exit(1); // 오류 종료
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php
-echo "Hello World!";
-
-$host = "mysql:";
-$host .= "host=localhost";
-$host .= ";dbname=shop";
-$host .= ";charser=utf8";
-
-$dbuser = "db2020";
-$dbpassword = "123456";
-
-if (extension_loaded("PDO") && extension_loaded("pdo_mysql")) {
-    try {
-        $conn = new PDO($host, $dbuser, $dbpassword);
-        echo "데이터 베이스 접속 성공!\n";
-
-    } catch (PDOException $e) {
-        echo "접속 실패\n";
-        echo $e->getMessage();
-    }
-} else {
-    echo "PDO 드라이버가 활성화 되어 있지 않습니다.\n";
-    exit(1); // 오류 종료
-}
-
 require "Modules\Databases\Database.php";
 $db = new \Modules\Databases\Database();
