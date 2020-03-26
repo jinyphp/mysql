52faf9f9352ed0a7c69b242c116b27589a81d016
hojinlee <infohojin@gmail.com>
2020-03-26 18:41:57 +0900

    PDO 접속1
    PDO 초기화 및 접속설정

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 echo "Hello World!";
+
+$host = "mysql:";
+$host .= "host=localhost";
+$host .= ";dbname=shop";
+$host .= ";charser=utf8";
+
+$dbuser = "db2020";
+$dbpassword = "123456";
+
+$conn = new PDO($host, $dbuser, $dbpassword);
