bef6bdd4de7cfcff1d2c960f211560003ca4c800
hojinlee <infohojin@gmail.com>
2020-03-26 18:48:18 +0900

    PDO 접속3
    PDO 드라이버 모듈 체크

--- index.php
+++ index.php
@@ -9,11 +9,16 @@ $host .= ";charser=utf8";
 $dbuser = "db2020";
 $dbpassword = "123456";
 
-try {
-    $conn = new PDO($host, $dbuser, $dbpassword);
-    echo "데이터 베이스 접속 성공!\n";
-    
-} catch (PDOException $e) {
-    echo "접속 실패\n";
-    echo $e->getMessage();
+if (extension_loaded("PDO") && extension_loaded("pdo_mysql")) {
+    try {
+        $conn = new PDO($host, $dbuser, $dbpassword);
+        echo "데이터 베이스 접속 성공!\n";
+
+    } catch (PDOException $e) {
+        echo "접속 실패\n";
+        echo $e->getMessage();
+    }
+} else {
+    echo "PDO 드라이버가 활성화 되어 있지 않습니다.\n";
+    exit(1); // 오류 종료
 }
