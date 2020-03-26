ad389491a41f41ec7c60ce6d83ad64b45bb94815
hojinlee <infohojin@gmail.com>
2020-03-26 18:47:22 +0900

    PDO 접속2
    데이터베이스 접속 성공여부 체크

--- index.php
+++ index.php
@@ -9,4 +9,11 @@ $host .= ";charser=utf8";
 $dbuser = "db2020";
 $dbpassword = "123456";
 
-$conn = new PDO($host, $dbuser, $dbpassword);
+try {
+    $conn = new PDO($host, $dbuser, $dbpassword);
+    echo "데이터 베이스 접속 성공!\n";
+    
+} catch (PDOException $e) {
+    echo "접속 실패\n";
+    echo $e->getMessage();
+}
