3b5cfae435ad691622bef4870e02cf08e8f31db8
hojinlee <infohojin@gmail.com>
2020-04-20 18:40:56 +0900

    docs/index 추가

---
+++ sample/sql/rawsql-02.php
@@ -0,0 +1,13 @@
+<?php
+require "../loading.php"; // 오토로딩
+
+// 데이터베이스 설정값
+$dbinfo = include("../dbinfo.php");
+
+// 설정값, 생성자 인자값으로 전달합니다.
+$db = new \Jiny\Mysql\Connection($dbinfo);
+
+// RawSQL 쿼리 예제
+$query = "SHOW DATABASES";
+$db->query($query); 
+print_r($db->fetchObjAll());
